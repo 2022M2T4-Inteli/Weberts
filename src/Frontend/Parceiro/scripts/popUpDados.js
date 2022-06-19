8a1c7974516df6f681f13693f670c0a5ff8737e1
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2022-06-18 23:34:36 -0300

    Updates Front-end 😛

---
+++ src/Frontend/Parceiro/scripts/popUpDados.js
@@ -0,0 +1,15 @@
+function abrir(){
+    
+    document.getElementById("popUpDados").style.display = "block";
+    document.getElementById("main-body").style.filter = "blur(10px) brightness(90%)";
+    document.getElementById("header").style.filter = "blur(10px) brightness(90%)";
+    
+}
+
+// function fechar(){
+
+//     document.getElementById("popUpDados").style.display = "none";
+//     document.getElementById("main-body").style.filter = "none";
+//     document.getElementById("header").style.filter = "none";
+
+// }
