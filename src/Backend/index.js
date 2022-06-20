4c7940769b761f77a342ff6b6049db4720c2862d
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2022-06-20 15:07:26 -0300

    Updates Front-End 🤩

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static("../frontend/Adm"));
+app.use(express.static("../frontend/Parceiro"));
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
